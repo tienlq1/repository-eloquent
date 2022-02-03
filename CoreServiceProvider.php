1d6f0808668bf2eb452c8a58859494b01a4db00a
tienlq@gmail.com <quangtienvkt@gmail.com>
2022-02-04 01:19:33 +0700

    nit

---
+++ CoreServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+namespace tienlq1\repository;
+
+use Illuminate\Support\ServiceProvider;
+
+
+class CoreServiceProvider extends ServiceProvider{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
